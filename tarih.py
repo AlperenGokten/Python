2370d0010a46d0eb1850a33ea82e792acd771c05
AlperenGokten <154828704+AlperenGokten@users.noreply.github.com>
2023-12-27 20:24:45 +0300

    int print
    int ve print dosyalarının nasıl kullanıcagını ve nasıl kullanılması gerektigii öğrendim

---
+++ tarih.py
@@ -0,0 +1,3 @@
+Dogumtarıhı  = input("bir tarih gir")
+
+print("Demek 14.12.2009 yılında doğdun")
---
+++ toplama.py
@@ -0,0 +1,5 @@
+sayıları = input("bir sayı gir:  ")
+toplanan =input ("yapılıcak işlem:   ")
+sonuç = input (" çıkan sonuç:   " )
+
+öğrendğimiz sonuç = input(" input komutu klavyeden  metinsel bilgi alır:   ")
