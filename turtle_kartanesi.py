6a8e769d26cdda853950bfb6c2cb4378211de0bb
Alperen <154828704+AlperenGokten@users.noreply.github.com>
2024-01-05 21:14:34 +0300

    turtle kartanesi
    turtle komutları ile yıldızlar ile kartanesi oluşturdum

---
+++ turtle_kartanesi.py
@@ -0,0 +1,16 @@
+import turtle
+
+turtle.color("blue")
+turtle.speed(100)
+for i in range(8):
+ for j in range(4):
+    turtle.forward(50)
+    turtle.right(90)
+    turtle.right(49)
+    turtle.forward(16)
+
+ for j in range(4):
+    turtle.forward(25)
+    turtle.right(30)
+    turtle.forward(3)
+
