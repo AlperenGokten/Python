373b1b3a5b17917ef9a718e42faa750681329468
Alperen <154828704+AlperenGokten@users.noreply.github.com>
2024-01-10 21:12:17 +0300

    zaman dilimi
    saat yıl ay gibi kavramları bilgi alarak mevcut olarak consola yazdırması

---
+++ date_tıme3.py
@@ -0,0 +1,10 @@
+today1 = date.today()
+
+print("Yıl:", today.year)
+print("Ay :", today.month)
+print("Gün:", today.day)
+
+yıl = int(input ("Doğum yılın nedir?"))
+print ("Sen :", today1.year-yıl,"yaşındasın.")
+print ("Sen :", (today1.year-yıl)*12,"ay yaşamışsın.")
+print ("Sen :", (today1.year-yıl)*365,"gün yaşamışsın.")
