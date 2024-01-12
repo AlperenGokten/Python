9bdd2f6d3f5354ae81fd2b0be8ff4252e8068eb2
Alperen <154828704+AlperenGokten@users.noreply.github.com>
2024-01-12 20:20:12 +0300

    dizeler
    python dizeler sınıflandırma konusunda seçim yapma gibi komutların nasıl kullanılacağı

---
+++ dizi2.py
@@ -0,0 +1,5 @@
+meyveler = ["elma","armut","çilek"]
+print (meyveler)
+
+for a in meyveler:
+    print (a)
---
+++ dizi3.py
@@ -0,0 +1,14 @@
+meyveler = ["elma","armut","çilek"]
+sebzeler = ["kereviz","lahana","pirasa"]
+print (meyveler)
+meyveler.append("muz")
+print(meyveler)
+meyveler.append("karpuz")
+print(meyveler)
+meyveler.append("erik")
+
+alinacaklar = meyveler + sebzeler 
+print(alinacaklar)
+
+alinacaklar.pop(3)
+print(alinacaklar)
