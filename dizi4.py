84830f0448be017942769d26c6f7e470f6cb93e2
Alperen <154828704+AlperenGokten@users.noreply.github.com>
2024-01-13 19:25:32 +0300

    diziler
    sınıflar bölümler

---
+++ dizi4.py
@@ -0,0 +1,15 @@
+meyveler = ["elma","armut","çilek"]
+sebzeler = ["kereviz","lahana","pirasa"]
+print (meyveler)
+meyveler.append("muz")
+print(meyveler)
+meyveler.append("karpuz")
+print(meyveler)
+meyveler.append("erik")
+
+alinacaklar = meyveler + sebzeler 
+print(alinacaklar)
+
+alinacaklar.pop(3)
+print(alinacaklar)
+
