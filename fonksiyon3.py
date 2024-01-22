427862e25cc493a6305afedb58030f5224ab20c5
Alperen <154828704+AlperenGokten@users.noreply.github.com>
2024-01-22 20:45:59 +0300

    fonksiyonlar

---
+++ fonksiyon3.py
@@ -0,0 +1,21 @@
+araclar1 = ["ferrari","Doğan","bmw"]
+araclar2 = ("Şahin","TOGG","Ford")
+araclar3 = {"Mercedes","Mazda","Volvo"} # set. indexi yok
+araclar4 = {"Kıvanç":"Hundai","Uğurhan":"Mercedes","Ahmet":"Honda"} # dictionary index yerine key
+
+
+
+print(type(araclar1))
+print(type(araclar2))
+print(type(araclar3))
+print(type(araclar4))
+
+print(araclar1)
+print(araclar2)
+print(araclar3)
+print(araclar4)
+
+print(araclar1[1])
+print(araclar2[1])
+print(araclar3[1])
+print(araclar4[1])
