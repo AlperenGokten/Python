243b6230d4725e7f954261a78aa23fc7e59e7492
Alperen <154828704+AlperenGokten@users.noreply.github.com>
2024-02-09 20:38:07 +0300

    masaüstü.uyg

---
+++ deneme3.py
@@ -0,0 +1,9 @@
+from PyQt6.QtWidgets import *
+app = QApplication([])
+window = QWidget()
+window.show()
+
+abd = QWidget()
+abd.show()
+
+
---
+++ deneme4.py
@@ -0,0 +1,14 @@
+import sys
+
+from PyQt6.QtWidgets import *
+app = QApplication(sys.argv)
+window = QPushButton("Tıkla")
+window.show()
+x = QWidget()  
+window1.show()  
+aa = QLabel("Merhaba")
+aa.show()
+
+app.exec()
+
+app.exec()
