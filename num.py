bb1e444d6686ca47fa19b2bfd9efbfb790cbd97a
Alperen <154828704+AlperenGokten@users.noreply.github.com>
2024-02-16 21:06:03 +0300

    Add files via upload

---
+++ num.py
@@ -0,0 +1,16 @@
+import numpy
+print (numpy.__version__)
+
+import numpy
+npdizisi = numpy.array([1,2,3,4,5])
+print(npdizisi)
+
+import numpy as np
+arr = np.array([1,2,3,])
+print(arr)
+
+print(type(npdizisi))
+print(type(arr))
+
+print(npdizisi.dtype)
+print(arr.dtype)
